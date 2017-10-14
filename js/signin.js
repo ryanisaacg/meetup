d01daf6d3bcadb1d52cb1ac5d8e5355b326dbdb8
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-14 18:20:55 -0400

    Fixed how signin works

--- js/signin.js
+++ js/signin.js
@@ -1,13 +1,13 @@
-
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token
     var xhr = new XMLHttpRequest()
     xhr.open('POST', 'signin.html')
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
     xhr.onload = function() {
-    console.log("onload")
         Cookies.set('id', id_token)
-        window.location.replace('https://google.com')
+        document.title = "Meet Up"
+        document.getElementById('sign-in-container').style.display = 'none'
+        document.getElementById('content-container').style.display = 'block'
     };
     xhr.send(id_token)
 }
