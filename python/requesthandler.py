e8cd3743307d870c1399233b88cc8a2e39aefb9b
Andrew Cater <acater@gmail.com>
2017-10-14 15:27:47 -0400

    Changes to request

--- python/requesthandler.py
+++ python/requesthandler.py
@@ -1,1 +1,8 @@
-class http.server.BaseHTTPRequestHandler(request,client_address,server)
+from http.server import SimpleHTTPRequestHandler
+from data_manager import DataManager
+import json
+manager = DataManager()
+class Handler(SimpleHTTPRequestHandler):
+    def do_Post(self):
+        length = int(self,.headers['content-length'])
+        data_string = str(self.rfile.read(length))
