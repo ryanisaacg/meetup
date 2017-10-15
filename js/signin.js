28112a8194323b96f6df73bac07965f689ce71ae
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-14 22:59:10 -0400

    Refactored and modified how the POST requests works

--- js/signin.js
+++ js/signin.js
@@ -1,13 +1,12 @@
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token
+    post_request('sign_in', { 'token': id_token }, () => {})
+}
+
+function post_request(url, contents, response_callback) {
     var xhr = new XMLHttpRequest()
     xhr.open('POST', 'signin.html')
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-    xhr.onload = function() {
-        Cookies.set('id', id_token)
-        document.title = "Meet Up"
-        document.getElementById('sign-in-container').style.display = 'none'
-        document.getElementById('content-container').style.display = 'block'
-    };
+    xhr.onload = () => response_callback(JSON.parse(xhr.responseText))
     xhr.send(id_token)
 }
