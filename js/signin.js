eb3f7d1025c81ea94b905c45008d0d165d4fc266
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-14 23:20:04 -0400

    Added sign in handling

--- js/signin.js
+++ js/signin.js
@@ -1,12 +1,14 @@
 function onSignIn(googleUser) {
+    const token = googleUser.getAuthResponse().id_token
+    const name = googleUser.getBasicProfile().getName()
     var id_token = googleUser.getAuthResponse().id_token
-    post_request('sign_in', { 'token': id_token }, () => {})
+    post_request('sign_in', { 'token': token, 'username': name }, (response) => { console.log(response) })
 }
 
 function post_request(url, contents, response_callback) {
     var xhr = new XMLHttpRequest()
-    xhr.open('POST', 'signin.html')
+    xhr.open('POST', url)
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
     xhr.onload = () => response_callback(JSON.parse(xhr.responseText))
-    xhr.send(id_token)
+    xhr.send(JSON.stringify(contents))
 }
