4b56eef707963d21ec3a7c20c912f4214ae56818
Ryan Goldstein <ryanisaacg@gmail.com>
2017-10-14 23:33:48 -0400

    Did some page transforms for when the user is logged in

--- js/requests.js
+++ js/requests.js
@@ -2,7 +2,11 @@ function onSignIn(googleUser) {
     const token = googleUser.getAuthResponse().id_token
     const name = googleUser.getBasicProfile().getName()
     var id_token = googleUser.getAuthResponse().id_token
-    post_request('sign_in', { 'token': token, 'username': name }, (response) => { console.log(response) })
+    post_request('sign_in', { 'token': token, 'username': name }, (response) => {
+        document.title = "Meet Up!"
+        document.getElementById('sign-in-container').style.display = 'none'
+        document.getElementById('content-container').style.display = 'block'
+    })
 }
 
 function post_request(url, contents, response_callback) {
